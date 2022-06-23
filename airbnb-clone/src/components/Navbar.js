1778900f75f318b9e11d9b2b84f623c2c27e841c
abdallah-alhasan <eng.abdallahalhasan@gmail.com>
2022-06-24 01:50:12 +0300

    Done

---
+++ airbnb-clone/src/components/Navbar.js
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function Navbar() {
+    return (
+        <nav>
+            <img src="https://i.pinimg.com/originals/3c/bf/be/3cbfbe148597341fa56f2f87ade90956.png" className="nav--logo" />
+        </nav>
+    )
+}
